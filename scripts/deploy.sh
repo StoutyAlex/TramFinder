8c9dc41ed899eb979bd9b49f2cb5b61ba63af40c
Alex Stout <alexstout009@hotmail.com>
2019-03-14 16:12:58 +0000

    updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 set -e
 
 ENVIRONMENT=""
@@ -16,7 +18,7 @@ done
 
 deploy() {
   echo "Creating deployment for ${ENVIRONMENT} to ${FUNCTION_NAME} lambda..."
-  AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://function.zip --region eu-west-1
+  aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://function.zip --region eu-west-1
 }
 
 case "${ENVIRONMENT}" in
