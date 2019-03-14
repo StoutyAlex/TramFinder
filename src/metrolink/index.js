629689fa1ed72b754606ceb6f332a02efb1a2535
Alex Stout <alexstout009@hotmail.com>
2019-03-14 18:06:09 +0000

    added metrolink environment variable

--- src/metrolink/index.js
+++ src/metrolink/index.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
-const config = require('../config');
 
 const getData = () =>
   axios.get('https://api.tfgm.com/odata/Metrolinks', {
     headers: {
-      'Ocp-Apim-Subscription-Key': config.TFGM.primaryKey,
+      'Ocp-Apim-Subscription-Key': process.env.metrolink_key,
     }
   });
 
