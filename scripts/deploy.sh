6ca3763af36c463697125efe6a55aca3380f90f6
Alex Stout <alexstout009@hotmail.com>
2019-03-14 18:26:19 +0000

    updated deploy function

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,8 +17,17 @@ while getopts ":e:" opt; do
 done
 
 deploy() {
-  echo "Creating deployment for ${ENVIRONMENT} to ${FUNCTION_NAME} lambda..."
-  aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://function.zip --region eu-west-1
+    echo "Creating deployment for ${ENVIRONMENT} to ${FUNCTION_NAME} lambda..."
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws configure set region eu-west-1
+
+    aws s3 rm s3://temp.bucket.tram-finder-lambda --recursive
+
+    aws s3 cp function.zip s3://temp.bucket.tram-finder-lambda
+
+    aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket temp.bucket.tram-finder-lambda --s3-key function.zip
+
 }
 
 case "${ENVIRONMENT}" in
