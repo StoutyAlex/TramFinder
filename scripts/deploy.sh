0bff845b699c81b9ff5afa6de05fde5743f4d42d
Alex Stout <alexstout009@hotmail.com>
2019-03-14 15:29:37 +0000

    Added region

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,7 @@ done
 
 deploy() {
   echo "Creating deployment for ${ENVIRONMENT} to ${FUNCTION_NAME} lambda..."
-  aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://function.zip
+  aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://function.zip --region eu-west-1
 }
 
 case "${ENVIRONMENT}" in
